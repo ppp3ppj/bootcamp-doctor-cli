66a96ca276fef6537a0a7cedeb47c8bdbf4e62b9
ppp3ppj <ppp.3ppj@gmail.com>
2024-09-01 22:30:43 +0700

    refactor make it more concise and idiomatic resource packages

--- internal/resources/packages.go
+++ internal/resources/packages.go
@@ -6,22 +6,10 @@ type PackageDoctor struct {
 }
 
 var packages = []PackageDoctor{
-    PackageDoctor{
-        Name: "golang",
-        VersionCommand: "go version",
-    },
-    PackageDoctor{
-        Name: "node",
-        VersionCommand: "node -v",
-    },
-    PackageDoctor{
-        Name: "zig",
-        VersionCommand: "zig version",
-    },
-    PackageDoctor{
-        Name: "ruby",
-        VersionCommand: "ruby -v",
-    },
+    {"golang", "go version"},
+    {"node", "node -v"},
+    {"zig", "zig version"},
+    {"ruby", "ruby -v"},
 }
 
 func GetPackages() []PackageDoctor {
