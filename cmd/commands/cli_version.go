97bdec5d9cffef14b8553d7a4348863fd997774c
ppp3ppj <ppp.3ppj@gmail.com>
2024-11-16 22:00:33 +0700

    add test for domain application and e2e version feature

--- cmd/commands/cli_version.go
+++ cmd/commands/cli_version.go
@@ -1,9 +1,12 @@
 package commands
 
 import (
+	"bytes"
 	"fmt"
+	"testing"
 
 	application "github.com/ppp3ppj/bootcamp-doctor-cli/application/cli-version"
+	domain "github.com/ppp3ppj/bootcamp-doctor-cli/domain/cli-version"
 	"github.com/spf13/cobra"
 )
 
@@ -16,4 +19,32 @@ func NewCmdCLIVersion(useCase *application.CLIVersionUseCase) *cobra.Command {
 			fmt.Printf("CLI Version: %s\nBuild: %s\n", cliVersion.Version, cliVersion.Build)
         },
     }
+}
+
+
+func TestVersionCmd(t *testing.T) {
+    version := domain.NewCLIVersion("1.0.0", "build1234")
+    useCase := &application.CLIVersionUseCase{Version: version}
+    command := NewCmdCLIVersion(useCase)
+
+    var output bytes.Buffer
+    command.SetOut(&output)
+
+    err := command.Execute()
+
+    if err != nil {
+        t.Fatalf("Expected no error, but got %v", err)
+    }
+
+    if !contains(output.String(), "CLI Version: 1.0.0") {
+        t.Errorf("Expected 'CLI Version: 1.0.0', but got %s", output.String())
+    }
+
+    if !contains(output.String(), "Build: build1234") {
+        t.Errorf("Expected 'Build: build1234', but got %s", output.String())
+    }
+}
+
+func contains(str, substr string) bool {
+    return len(str) >= len(substr) && str[:len(substr)] == substr
 }
---
+++ domain/cli-version/cli_version_test.go
@@ -0,0 +1,15 @@
+package domain
+
+import "testing"
+
+func TestCLIVersionCreation(t *testing.T) {
+    version := NewCLIVersion("1.0.0", "build1234")
+
+    // Test if the values are set correctly
+	if version.Version != "1.0.0" {
+		t.Errorf("Expected version '1.0.0', but got '%s'", version.Version)
+	}
+	if version.Build != "build1234" {
+		t.Errorf("Expected build 'build1234', but got '%s'", version.Build)
+	}
+}
